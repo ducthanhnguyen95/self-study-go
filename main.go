193ce88c8f98190036f06715d1d3a19b7fd213b6
thanhnd <ducthanhnguyen95@gmail.com>
2025-01-23 17:05:48 +0700

    Update

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"github.com/ducthanhnguyen95/self-study-go/magazine"
-)
+import "fmt"
+
+type Liters float64
+type Gallons float64
+
+type Title string
 
 func main() {
 	//var s magazine.Subscriber
@@ -64,22 +66,50 @@ func main() {
 	//fmt.Println("State:", employee.Address.State)
 	//fmt.Println("Postal Code:", employee.Address.PostalCode)
 
-	subscriber := magazine.Subscriber{Name: "Aman Singh"}
-	subscriber.Street = "123 Oak St"
-	subscriber.City = "Omaha"
-	subscriber.State = "NE"
-	subscriber.PostalCode = "68111"
-	fmt.Println("Street:", subscriber.Street)
-	fmt.Println("City:", subscriber.City)
-	fmt.Println("State:", subscriber.State)
-	fmt.Println("Postal Code:", subscriber.PostalCode)
-	employee := magazine.Employee{Name: "Joy Carr"}
-	employee.Street = "456 Elm St"
-	employee.City = "Portland"
-	employee.State = "OR"
-	employee.PostalCode = "97222"
-	fmt.Println("Street:", employee.Street)
-	fmt.Println("City:", employee.City)
-	fmt.Println("State:", employee.State)
-	fmt.Println("Postal Code:", employee.PostalCode)
+	//subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	//subscriber.Street = "123 Oak St"
+	//subscriber.City = "Omaha"
+	//subscriber.State = "NE"
+	//subscriber.PostalCode = "68111"
+	//fmt.Println("Street:", subscriber.Street)
+	//fmt.Println("City:", subscriber.City)
+	//fmt.Println("State:", subscriber.State)
+	//fmt.Println("Postal Code:", subscriber.PostalCode)
+	//employee := magazine.Employee{Name: "Joy Carr"}
+	//employee.Street = "456 Elm St"
+	//employee.City = "Portland"
+	//employee.State = "OR"
+	//employee.PostalCode = "97222"
+	//fmt.Println("Street:", employee.Street)
+	//fmt.Println("City:", employee.City)
+	//fmt.Println("State:", employee.State)
+	//fmt.Println("Postal Code:", employee.PostalCode)
+
+	//var carFuel Gallons
+	//var busFuel Liters
+	//carFuel = Gallons(10.0)
+	//busFuel = Liters(240.0)
+	//fmt.Println(carFuel, busFuel)
+
+	//carFuel = Gallons(Liters(40.0))
+	//busFuel = Liters(Gallons(63.0))
+	//fmt.Printf("Gallons: %0.1f Liters: %0.1f\n", carFuel, busFuel)
+	//
+	//carFuel = Gallons(Liters(40.0) * 0.264)
+	//busFuel = Liters(Gallons(63.0) * 3.785)
+	//fmt.Printf("Gallons: %0.1f Liters: %0.1f\n", carFuel, busFuel)
+
+	//fmt.Println(Liters(1.2) + Liters(3.4))
+	//fmt.Println(Gallons(5.5) - Gallons(2.2))
+	//fmt.Println(Liters(2.2) / Liters(1.1))
+	//fmt.Println(Gallons(1.2) == Gallons(1.2))
+	//fmt.Println(Liters(1.2) < Liters(3.4))
+	//fmt.Println(Liters(1.2) > Liters(3.4))
+
+	fmt.Println(Title("Alien") == Title("Alien"))
+	fmt.Println(Title("Alien") < Title("Zodiac"))
+	fmt.Println(Title("Alien") > Title("Zodiac"))
+	fmt.Println(Title("Alien") + "s")
+	//fmt.Println(Title("Jaws 2") - " 2")
+
 }
